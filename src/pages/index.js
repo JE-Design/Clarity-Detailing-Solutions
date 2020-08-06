1a290dfb902e84849ca3b700ee865895c5601064
Liannus <josh-gorman@hotmail.com>
2020-08-05 22:45:18 -0400

    Add boilerplate for new pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as contactPage } from "./contactPage";
+export { default as landingPage } from "./landingPage";
+export { default as servicesPage } from "./servicesPage";
+export { default as testimonialsPage } from "./testimonialsPage";
