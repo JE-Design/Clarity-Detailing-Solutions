99de854e699fc7f44eb8e01b50d707c21c18cb68
Liannus <josh-gorman@hotmail.com>
2020-08-09 21:44:13 -0400

    Added font and image boilerplating

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,1 @@
-export { default as landing_image } from "./landing_image.jpg";
+export { default as landing_image } from "./images/landing_image.jpg";
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,10 +4,13 @@ import "./LandingPage.scss";
 
 const LandingPage = () => (
   <div
-    className="w-full landing-page"
+    className="w-full flex landing-page"
     style={{ backgroundImage: `url(${landing_image})` }}
   >
-    <p>hello</p>
+    <div className="self-end">
+      <h1 class="text-4xl">Clarity Detailing Solutions</h1>
+      <h2 class="text-3xl">It's all in the details</h2>
+    </div>
   </div>
 );
 
