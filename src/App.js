1c200b7a41b80bf42b3ae9267383b9a2f4166622
ForkBombGIT <erosdipede@gmail.com>
2020-08-11 06:35:30 -0400

    Add basic navbar

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import {
   ServicesPage,
   TestimonialsPage,
 } from "pages/";
+import { Navbar } from "components";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <LandingPage />
       <ContactPage />
       <ServicesPage />
