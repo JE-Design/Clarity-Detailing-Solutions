d145ae012af415e0b6790141d47acd5bab5657ba
Josh <19962651+Liannus@users.noreply.github.com>
2020-08-17 22:58:08 -0400

    CDS-9 Add card for catch us here on contact page (#22)
    Changelog:
    - Added `TextIconCombo` and `IconLink` components for their respective use cases
    - Added simple full width card to contact page
    - Added fontAwesome packages to use icons throughout the project
    
    Other Concerns:
    - Not completion of the CDS-9 ticket, some components and code samples to see font-awesome. Will be completed with merging of CDS-7 and the implementation of page headers.
    
    Co-authored-by: Eros Di Pede <34588189+ForkBombGIT@users.noreply.github.com>

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
+export { default as LinkIcon } from "./LinkIcon";
+export { default as TextIconCombo } from "./TextIconCombo";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
