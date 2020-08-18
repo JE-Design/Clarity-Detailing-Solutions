4ff7f40cc6906a4e105bdb6bf37435bbf001f789
Josh <19962651+Liannus@users.noreply.github.com>
2020-08-18 18:57:55 -0400

    CDS-26 Add contact section to landing page (#27)
    Changelog:
    - Added contact section to the landing page
    
    Notes:
    - Very similar to the current code used for landing page although there are different rules for:
      - background image
      -  alignment
      -  header values
    Could be turned into a text component or at least maybe minimized slightly?

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { landing_image } from "assets/";
+import { landing_image, contact_image } from "assets/";
+import variables from "styles/colors.scss";
 import "./LandingPage.scss";
 
 const LandingPage = () => (
-  <div
-    className="w-full flex landing-page bg-cover bg-right"
-    style={{ backgroundImage: `url(${landing_image})` }}
-  >
-    <div className="p-6 self-end text-left">
-      <h1>Clarity Detailing Solutions</h1>
-      <h2>It's all in the details</h2>
+  <div>
+    <div
+      id="landing-header"
+      className="w-full flex bg-cover bg-right"
+      style={{ backgroundImage: `url(${landing_image})` }}
+    >
+      <div className="p-6 self-end text-left">
+        <h1>Clarity Detailing Solutions</h1>
+        <h2>It's all in the details</h2>
+      </div>
+    </div>
+    <div
+      id="contact-section"
+      className="w-full flex justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, ${variables.colorBlack}, transparent 4%, transparent 96%, ${variables.colorBlack}), url(${contact_image})`,
+      }}
+    >
+      <div className="p-6 self-center">
+        <h3>Need a Quote?</h3>
+        <a href="/contact">
+          <h4>Contact Us</h4>
+        </a>
+      </div>
     </div>
   </div>
 );
