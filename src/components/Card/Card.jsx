c84b4eb5e50d3b69ed6948bbe7c8ce29c23f8bfa
Josh <19962651+Liannus@users.noreply.github.com>
2020-08-19 23:41:05 -0400

    Add code for google maps API embed (#32)
    Changelog:
    - Added flex auto to cards to allow for equal height when in series
    - Added base for google map embed to fully fill card while also not pushing API key to vcs
    
    resolves #10

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Card.scss";
 
-const Card = ({ children, className }) => {
+const Card = ({ children, className, empty }) => {
   return (
     <div
       className={`${className} card relative z-10 p-8 flex text-left flex-col`}
     >
-      <div className="inner-container -m-3 p-6">{children}</div>
+      <div className={`inner-container flex-auto -m-3 ${empty ? "" : "p-6"}`}>
+        {children}
+      </div>
     </div>
   );
 };
