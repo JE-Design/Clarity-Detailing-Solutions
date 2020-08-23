d1a4f8821ba96ae41afef52cba5cefca60f7e576
Liannus <josh-gorman@hotmail.com>
2020-08-23 18:18:50 -0400

    Swapping object-fit cover

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -8,24 +8,24 @@ const Testimonial = ({
   customerTestimony,
   customerPhoto,
   carPhotos,
-  review
+  review,
 }) => (
   <Card className={className}>
     <div className="testimony-content">
       <div className="flex">
         <h3 className="w-11/12">{customerName}</h3>
         <img
-          className="rounded-full h-16 w-16 mb-2"
+          className="rounded-full h-16 w-16 mb-2 object-cover"
           src={customerPhoto}
           alt="customer"
         ></img>
       </div>
       <p>{customerTestimony}</p>
       <a href={review.url}>{review.text}</a>
-      <div id="car-images">
-      {carPhotos.map((photoPath) => {
-        return <img className="w-full mt-5" alt="car" src={photoPath}></img>;
-      })}
+      <div>
+        {carPhotos.map((photoPath) => {
+          return <img className="w-full mt-5" alt="car" src={photoPath}></img>;
+        })}
       </div>
     </div>
   </Card>
