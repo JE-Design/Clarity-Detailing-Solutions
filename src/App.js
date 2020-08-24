c9dd0534b6ffef7a5baa4d5a35d631b279cad060
ForkBombGIT <erosdipede@gmail.com>
2020-08-24 13:06:57 -0400

    Add color transitioning
    Add styling to nav depending on page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
 import React from "react";
-import AppRouter from "./AppRouter"
-import Footer from "./components/Footer"
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./AppRouter";
+import Footer from "./components/Footer";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faInstagram,
-  faFacebook,
-  faTwitter,
-  faYoutube,
-  faSnapchat,
+    faInstagram,
+    faFacebook,
+    faTwitter,
+    faYoutube,
+    faSnapchat,
 } from "@fortawesome/free-brands-svg-icons";
 import {
-  faEnvelope,
-  faMapMarker,
-  faPhone,
+    faEnvelope,
+    faMapMarker,
+    faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 import { Navbar } from "components";
 
 //Initialize fontawesome with desired icons
 library.add(
-  faInstagram,
-  faFacebook,
-  faTwitter,
-  faYoutube,
-  faSnapchat,
-  faEnvelope,
-  faMapMarker,
-  faPhone
+    faInstagram,
+    faFacebook,
+    faTwitter,
+    faYoutube,
+    faSnapchat,
+    faEnvelope,
+    faMapMarker,
+    faPhone
 );
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <AppRouter />
-      <Footer />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <AppRouter />
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import "./Navbar.scss";
 
 import { logo_image } from "assets/";
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const [navbarColor, setNavbarColor] = useState("#10101000");
+    const location = useLocation();
+
+    // update navbar color on scroll
+    window.addEventListener("scroll", () => {
+        // if scroll is not at 0
+        if (window.scrollY > 0) setNavbarColor(`#101010`);
+        // if scroll is at zero, make navbar transparent
+        else setNavbarColor("transparent");
+    });
     return (
-        <header className="z-20 fixed flex flex-col w-full">
+        <header
+            className="z-20 fixed flex flex-col w-full"
+            style={{
+                backgroundColor: navbarColor,
+                transition: "background-color 0.5s",
+            }}
+        >
             <nav className="relative flex w-full p-6">
                 <a href="/" className="w-1/3 sm:w-24 mr-4">
                     <img src={logo_image} alt="logo"></img>
@@ -13,13 +30,46 @@ const Navbar = () => {
                 <div id="nav-content" className="flex items-center">
                     <h2 className="pr-3">Clarity Detailing Solutions</h2>
                     <span className="h-16"></span>
-                    <a href="/services" className="p-3">
+                    <a
+                        href="/services"
+                        className="p-3"
+                        style={
+                            location.pathname === "/services"
+                                ? {
+                                      textDecoration: "underline",
+                                      fontWeight: "bold",
+                                  }
+                                : {}
+                        }
+                    >
                         Services
                     </a>
-                    <a href="/testimonials" className="p-3">
+                    <a
+                        href="/testimonials"
+                        className="p-3"
+                        style={
+                            location.pathname === "/testimonials"
+                                ? {
+                                      textDecoration: "underline",
+                                      fontWeight: "bold",
+                                  }
+                                : {}
+                        }
+                    >
                         Testimonials
                     </a>
-                    <a href="/contact" className="p-3">
+                    <a
+                        href="/contact"
+                        className="p-3"
+                        style={
+                            location.pathname === "/contact"
+                                ? {
+                                      textDecoration: "underline",
+                                      fontWeight: "bold",
+                                  }
+                                : {}
+                        }
+                    >
                         Contact
                     </a>
                 </div>
