a8df80a698b9572ec11d6eba6f3aae677ee8fde9
ForkBombGIT <erosdipede@gmail.com>
2020-08-30 00:48:02 -0400

    Add google analytics via react module

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
 import {
   BrowserRouter as Router,
   Switch,
@@ -23,9 +25,19 @@ function ScrollToTop() {
   return null;
 }
 
+const history = createBrowserHistory();
+
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
+
 function AppRouter() {
   return (
-    <Router>
+    <Router history={history}>
       <ScrollToTop />
       <Navbar />
       <Switch>
